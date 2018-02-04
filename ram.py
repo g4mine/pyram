6fc920ba22528d2fc085ced6154d555f5c171604
Gabriel Barone <gbarone2004@gmail.com>
2018-02-04 15:05:30 -0500

    Update ram.py

--- ram.py
+++ ram.py
@@ -1,12 +1,12 @@
-import os
-import base64
-inp = input (str ())
+import os, base64
+inp = raw_input("Path to encode: ")
 filenames = os.listdir(inp)
 for arq in filenames:
-	pathname = os.path.join("data", arq)
+	pathname = os.path.join(inp, arq)
 	file = open(pathname, 'r')
 	text = file.read()
 	enc = base64.b64encode(text)
 	file = open(pathname, 'w')
 	file.write(enc)
 	file.close()
+
