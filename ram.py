c4c2ef359f4442524ac8a7f9df89ce637f48b0e0
Gabriel Barone <gbarone2004@gmail.com>
2018-02-04 12:52:23 -0200

    Update ram.py

--- ram.py
+++ ram.py
@@ -1,13 +1,11 @@
 import os
 import base64
 filenames = os.listdir("data")
-i = 0
-while i <= 1:
-	pathname = os.path.join("data", filenames[i])
+for arq in filenames:
+	pathname = os.path.join("data", arq)
 	file = open(pathname, 'r')
 	text = file.read()
 	enc = base64.b64encode(text)
 	file = open(pathname, 'w')
 	file.write(enc)
 	file.close()
-	i += 1
